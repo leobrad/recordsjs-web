a3bf161f23926d28ee28ba4c8522d985f56bf54e
Leo Brad <>
2023-09-10 00:05:36 +0800

    [Add]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import Home from '~/script/page/Home'
+import '~/style/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Home />);
